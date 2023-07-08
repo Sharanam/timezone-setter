0ff7973bff74af64844b521e270e51b6ffd8d2a3
ChotaiSharanam <sharanam.chotai@investisdigital.com>
2023-07-08 08:03:46 +0530

    minor typo fix

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ const isWindows = process.platform === "win32";
 // const isLinux = process.platform === "linux";
 // const isMac = process.platform === "darwin";
 
-const timeZones = require("./timeZones.json");
+const timezones = require("./timezones.json");
 let argument = process.argv[2];
 if (argument === "list" || argument === "help") {
   console.log("List of timezones: ");
-  console.log(Object.keys(timeZones).sort().join("\n"));
+  console.log(Object.keys(timezones).sort().join("\n"));
   return;
 }
-let timeZone = argument || "India";
+let timezone = argument || "India";
 
-if (!timeZones[timeZone]) {
-  const regex = new RegExp(timeZone, "i");
-  const matchedTimeZones = Object.keys(timeZones).filter((tz) =>
+if (!timezones[timezone]) {
+  const regex = new RegExp(timezone, "i");
+  const matchedTimezones = Object.keys(timezones).filter((tz) =>
     tz.match(regex)
   );
-  if (matchedTimeZones.length === 0) {
+  if (matchedTimezones.length === 0) {
     console.log("No time zone found");
     return;
-  } else if (matchedTimeZones.length === 1) {
-    timeZone = matchedTimeZones[0];
+  } else if (matchedTimezones.length === 1) {
+    timezone = matchedTimezones[0];
   } else {
     console.log("Multiple time zones found");
-    console.log(matchedTimeZones.join(", "));
+    console.log(matchedTimezones.join(", "));
     return;
   }
 }
 if (isWindows)
   exec(
-    `powershell.exe -Command "Set-TimeZone -Id '${timeZones[timeZone]}'"`,
+    `powershell.exe -Command "Set-TimeZone -Id '${timezones[timezone]}'"`,
     (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
@@ -42,7 +42,7 @@ if (isWindows)
         return;
       }
       console.log(
-        stdout ? `System Output: ${stdout}` : "Time zone set to " + timeZone
+        stdout ? `System Output: ${stdout}` : "Time zone set to " + timezone
       );
     }
   );
